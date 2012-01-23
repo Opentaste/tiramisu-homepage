e1e2ac3a16f5e9512c3ce8f0b6076f83228b2f31
Gianluca Bargelli <g.bargelli@gmail.com>
2012-01-24 00:01:04 +0100

    First commit

---
+++ app/models.py
@@ -0,0 +1,6 @@
+"""
+Python Datastore API: http://code.google.com/appengine/docs/python/datastore/
+"""
+
+from google.appengine.ext import db
+
---
+++ app/views.py
@@ -0,0 +1,36 @@
+"""
+Flask Module Docs:  http://flask.pocoo.org/docs/api/#flask.Module
+
+This file is used for both the routing and logic of your
+application.
+"""
+
+from google.appengine.api import mail
+
+from flask import Module, url_for, render_template, request, redirect
+
+views = Module(__name__, 'views')
+
+
+@views.route('/')
+def index():
+    """Render website's index page."""
+    return render_template('index.html')
+    
+@views.route('/docs/')
+def docs():
+    """Render documentation page."""
+    return render_template('docs.html')
+
+
+@views.after_request
+def add_header(response):
+    """Add header to force latest IE rendering engine and Chrome Frame."""
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    return response
+
+
+@views.app_errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
